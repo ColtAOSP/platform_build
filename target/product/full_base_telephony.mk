7db7efbbf25742f72770cd28ffba2998c0235837
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-01-28 09:05:16 +0700

    Disable dataroaming by default
    The property is removed so that the property in vendor/lineage
    can take effect.
    
    Change-Id: Iefadc1a6e71cbf92ecb7363c953f4b5a82064446

--- target/product/full_base_telephony.mk
+++ target/product/full_base_telephony.mk
@@ -19,5 +19,8 @@
 # build quite specifically for the emulator, and might not be
 # entirely appropriate to inherit from for on-device configurations.
 
+PRODUCT_PROPERTY_OVERRIDES := \
+    keyguard.no_require_sim=true
+
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
