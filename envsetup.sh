CypherOS/build

roomservice: Updated roomservice
Change-Id: Ib1f661772a14bee3c81fde9459ce25e8ae51c0eb

envsetup.sh
@@ -659,6 +659,7 @@ function lunch()
         return 1
     fi
 
+	local device=$(echo -n $product | sed -e "s/.*aoscp_//")
     check_product $product
 
     TARGET_PRODUCT=$product \
@@ -670,11 +671,11 @@ function lunch()
 	    # If we can't find a product, try to grab it from our github
         T=$(gettop)
         pushd $T > /dev/null
-        build/tools/roomservice.py $product
+        build/tools/roomservice.py $device
         popd > /dev/null
         check_product $product
     else
-        build/tools/roomservice.py $product true
+        build/tools/roomservice.py $device
     fi
     if [ $? -ne 0 ]
     then

ColtAOSP/platform_build

envsetup.sh
@@ -669,6 +669,7 @@ function lunch()
         return 1
     fi
 
+	local device=$(echo -n $product | sed -e "s/.*colt_//")
     check_product $product
 
     TARGET_PRODUCT=$product \
@@ -680,11 +681,11 @@ function lunch()
 	    # If we can't find a product, try to grab it from our github
         T=$(gettop)
         pushd $T > /dev/null
-        build/tools/roomservice.py $product
+        build/tools/roomservice.py $device
         popd > /dev/null
         check_product $product
     else
-        build/tools/roomservice.py $product true
+        build/tools/roomservice.py $device
     fi
     if [ $? -ne 0 ]
     then