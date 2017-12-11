CypherOS/build

core: Include QCOM/MTK targets
Change-Id: I7e33db775e80f042ef71a43c67f32abd4b6038e7

core/config.mk
@@ -897,6 +897,12 @@ $(eval include vendor/aoscp/sepolicy/sepolicy.mk)
 
 # Include any vendor specific apicheck.mk file
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
+
+# Rules for QCOM targets
+-include $(TOPDIR)vendor/aoscp/build/core/qcom_target.mk
+
+# Rules for MTK targets
+-include $(TOPDIR)vendor/aoscp/build/core/mtk_target.mk
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk

ColtAOSP/platform_build

Signed-off-by: RakeshBatra <raakesh.batra@rediffmail.com>

core/config.mk
@@ -972,6 +972,12 @@ $(eval include vendor/colt/sepolicy/sepolicy.mk)
 
 # Include any vendor specific apicheck.mk file
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
+
+# Rules for QCOM targets
+-include $(TOPDIR)vendor/colt/build/core/qcom_target.mk
+
+# Rules for MTK targets
+-include $(TOPDIR)vendor/colt/build/core/mtk_target.mk
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk